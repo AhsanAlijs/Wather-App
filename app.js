e4807d7b55c26d9c57c2c6a13fac16c50b9382ec
Ahsan-ali001 <ahsanali45444@gmail.com>
2023-10-23 11:37:31 -0700

    New Function And Fully correct this code

--- app.js
+++ app.js
@@ -1,53 +1,62 @@
 const form = document.getElementById('form');
 const searchInput = document.getElementById('search');
 const div = document.getElementById('data');
-const divMiss = document.getElementById('error')
-const loading = document.getElementById('loading')
+const load = document.querySelector('.load')
+const found = document.querySelector('.errors')
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
-    div.innerHTML+=''
-    // loading.innerHTML = `
-    // <h2 class='loadingc'>Loading....</h2>
-    // `
+    // div.innerHTML = ""
+    found.innerHTML=''
+    load.style.display = 'block'
     let searchValue = searchInput.value;
-    
-    // div.innerHTML+=''
     try {
         const res = await axios.get(`https://api.weatherapi.com/v1/current.json?key=339c612e69db487da81155228231610&q=${searchValue}`)
         const data = res.data;
         console.log(data);
         div.innerHTML += `<div class="main-content">
-    <h2 class="h-name">${data.location.name}</h2>
-    <p class="time">${data.location.localtime} , ${data.location.country}</p>
-    <div class="c-data">
-    <div class="c-text"><p>${data.current.temp_c}<sup>o</sup>C</p></div>
-    <div class="c-icon"><img src="${data.current.condition.icon}"class="image-icon"></div>
-    </div>
-    <p class="c-last">${data.current.condition.text}</p>
-    </div>`
+            <h2 class="h-name">${data.location.name}</h2>
+            <p class="time">${data.location.localtime} , ${data.location.country}</p>
+            <div class="c-data">
+            <div class="c-text"><p>${data.current.temp_c}<sup>o</sup>C</p></div>
+            <div class="c-icon"><img src="${data.current.condition.icon}"class="image-icon"></div>
+            </div>
+            <p class="c-last">${data.current.condition.text}</p>
+            </div>`
     } catch (error) {
-        // divMiss.innerHTML= `
-        //     <p id='error'>
-        //     Data Not Found!
-        //     </p>
-        //     `;
-        alert('not city found')
+        found.innerHTML = `<h2 class="error">Not City Found</h2>`
     } finally {
-    //     loading.innerHTML = `
-    // <h2 class='loading'>Loading....</h2>
-    // `
+        load.style.display = 'none'
         searchInput.value = ''
-
     }
-
-
 });
 
 
+// form.addEventListener('submit', async (e) => {
+//     e.preventDefault();
+//     div.innerHTML+=''
+//     let searchValue = searchInput.value;
+//     try {
+//         const res = await axios.get(`https://api.weatherapi.com/v1/current.json?key=339c612e69db487da81155228231610&q=${searchValue}`)
+//         const data = res.data;
+//         console.log(data);
+//         div.innerHTML += `<div class="main-content">
+//     <h2 class="h-name">${data.location.name}</h2>
+//     <p class="time">${data.location.localtime} , ${data.location.country}</p>
+//     <div class="c-data">
+//     <div class="c-text"><p>${data.current.temp_c}<sup>o</sup>C</p></div>
+//     <div class="c-icon"><img src="${data.current.condition.icon}"class="image-icon"></div>
+//     </div>
+//     <p class="c-last">${data.current.condition.text}</p>
+//     </div>`
+//     } catch (error) {
+//        alert('54545454454')
+//     } finally {
 
+//         searchInput.value = ''
 
-
+//     }
+// });
 // .then((res) => {
 //     const data = res.data;
 //     // console.log(data);
@@ -72,3 +81,5 @@ form.addEventListener('submit', async (e) => {
 //     <h3 class='error'>City Or Country Not Found</h3>
 //     `
 // })
+
+
